9f28235698feffd2a406adae6f1bdef7664dc661
Kai Kitamura <namedpython+dev@gmail.com>
2018-09-22 21:27:33 +0900

    feat: extract inside contours

--- source.py
+++ source.py
@@ -9,32 +9,46 @@ if image is None:
   print("File not found.")
   exit()
 
+def write_out(img, name):
+  cv2.imwrite(f'{DIR}/image/result/{name}.png', img)
+
+def extract_inside(contours, hierarchy, debug=False):
+  extracted = []
+  contours_drawed = image.copy()
+  if debug:
+    print(f'len: {len(contours)}')
+    print(hierarchy)
+
+  for index in range(len(contours)):
+    if hierarchy[0][index][2] != -1 or cv2.contourArea(contours[index]) < 8000:
+      continue
+
+    extracted.append(contours[index])
+    
+    if debug:
+      cv2.drawContours(contours_drawed, contours, index, (255, 0, 255), 1)
+
+    print(f'{index} : {cv2.contourArea(contours[index])}')
+  
+  if debug:
+    write_out(contours_drawed, 'out_contours')
+  
+  return extracted
+
 width, height = image.shape[:2]
 
 # ret, image = cv2.threshold(image, 220, 255, cv2.THRESH_BINARY) # use threshold
 extracted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)[:, :, 1]
 ret, thrshld_ex = cv2.threshold(extracted, 50, 255, cv2.THRESH_BINARY)
 
+write_out(thrshld_ex, 'thrshld')
+
 kernel = np.ones((3, 1), np.uint8)
 thrshld_ex = cv2.morphologyEx(thrshld_ex, cv2.MORPH_OPEN, kernel)
 
-im_cont, contours, hierarchy = cv2.findContours(thrshld_ex, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+im_cont, contours, hierarchy = cv2.findContours(thrshld_ex, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-print(len(contours))
+inside_contours = extract_inside(contours, hierarchy, True)
 
-print(hierarchy)
+write_out(thrshld_ex, 'out')
 
-contours_drawed = image.copy()
-
-# cv2.drawContours(contours_drawed, contours, 10, (0, 0, 255))
-
-for index in range(len(contours)):
-  cv2.drawContours(contours_drawed, contours, index, (0, 0, 255), 1)
-  # index += 1
-
-# mask = np.zeros((width+2, height+2), np.uint8)
-# mask[:] = 0
-# cv2.floodFill(thrshld_ex, mask, (0, 0), (0, 0, 255))
-
-cv2.imwrite(f'{DIR}/image/out.png', thrshld_ex)
-cv2.imwrite(f'{DIR}/image/out_contours.png', contours_drawed)
