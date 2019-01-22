2ae960b47fd8296b42a3aa374dc582873077a6ef
Kai Kitamura <namedpython+dev@gmail.com>
2019-01-22 14:49:50 +0900

    follows flake8 and some configuration

--- source.py
+++ source.py
@@ -6,34 +6,37 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 
 image = cv2.imread(f'{DIR}/image/paper02.jpg')
 if image is None:
-  print("File not found.")
-  exit()
+    print("File not found.")
+    exit()
+
 
 def write_out(img, name):
-  cv2.imwrite(f'{DIR}/image/result/{name}.png', img)
+    cv2.imwrite(f'{DIR}/image/result/{name}.png', img)
+
 
 def extract_inside(contours, hierarchy, debug=False):
-  extracted = []
-  contours_drawed = image.copy()
-  if debug:
-    print(f'len: {len(contours)}')
-    print(hierarchy)
+    extracted = []
+    contours_drawed = image.copy()
+    if debug:
+        print(f'len: {len(contours)}')
+        print(hierarchy)
 
-  for index in range(len(contours)):
-    if hierarchy[0, index, 2] != -1 or cv2.contourArea(contours[index]) < 8000:
-      continue
+    for index in range(len(contours)):
+        if hierarchy[0, index, 2] != -1 or cv2.contourArea(contours[index]) < 8000:
+            continue
 
     extracted.append(contours[index])
-    
+
     if debug:
-      cv2.drawContours(contours_drawed, contours, index, (255, 0, 255), 1)
+        cv2.drawContours(contours_drawed, contours, index, (255, 0, 255), 1)
 
     print(f'{index} : {cv2.contourArea(contours[index])}')
-  
-  if debug:
-    write_out(contours_drawed, 'out_contours')
-  
-  return extracted
+
+    if debug:
+        write_out(contours_drawed, 'out_contours')
+
+    return extracted
+
 
 width, height = image.shape[:2]
 
@@ -52,19 +55,18 @@ inside_contours = extract_inside(contours, hierarchy, True)
 
 clr_img_cp = image.copy()
 for circle in inside_contours:
-  colors = []
-  av_color = 0
-  M = cv2.moments(circle)
-  center_of_circle = (int(M['m10']/M['m00']), int(M['m01']/M['m00']))
-  cnt = 0
-  for approx in circle:
-    if cnt == 20:
-      break
-    cv2.circle(clr_img_cp, (approx[0, 0] - 1, approx[0, 1] - 1), 1, (255, 0, 255), 1)
-    colors.append(image[approx[0, 1] - 1, approx[0, 0] - 1])
-    cnt+=1
-  cav = np.mean(colors, axis=0)
-  cv2.circle(clr_img_cp, center_of_circle, 100, cav, 20)
+    colors = []
+    av_color = 0
+    M = cv2.moments(circle)
+    center_of_circle = (int(M['m10']/M['m00']), int(M['m01']/M['m00']))
+    cnt = 0
+    for approx in circle:
+        if cnt == 20:
+            break
+        cv2.circle(clr_img_cp, (approx[0, 0] - 1, approx[0, 1] - 1), 1, (255, 0, 255), 1)
+        colors.append(image[approx[0, 1] - 1, approx[0, 0] - 1])
+        cnt += 1
+    cav = np.mean(colors, axis=0)
+    cv2.circle(clr_img_cp, center_of_circle, 100, cav, 20)
 
 write_out(clr_img_cp, 'out')
-
